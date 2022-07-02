de97e9f4011953645e6f801439e6b9761251b832
peoft <paulo.ostermann.filho@gmail.com>
2022-07-02 17:57:29 -0300

    Validando nulo e undefined

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,15 @@
 var result = "";
 
-function validateNumbers(number1, number2) {
-    var result = "Os numeros " + String(number1) + " e " + String(number2);
-    var soma = number1 + number2;
+function validateNumbers(number1, number2) {    
+    var result = "";
+    var soma = 0;
+
+    if (number1 === null || number1 === undefined  ||
+        number2 === null || number2 === undefined)
+        return "Defina dois numeros!";
+
+    result = "Os numeros " + String(number1) + " e " + String(number2);
+    soma = number1 + number2;
     if (number1 === number2) {
         result += " sao iguais.";
     } else {
@@ -23,5 +30,5 @@ function validateNumbers(number1, number2) {
 }
 
 
-result  = validateNumbers(5, 3);
+result  = validateNumbers(6, 15);
 console.log("resultado = ", result);
